e066d3d9a6be2bd894360c6dc272afa8d68f288c
Yulya Protasevich <Protasevich.Yuliya@gmail.com>
2019-08-21 12:40:09 +0700

    Added SwaggerUI.

--- Finances/Controllers/AccountsController.cs
+++ Finances/Controllers/AccountsController.cs
@@ -23,8 +23,13 @@ namespace Finances.Controllers
             this.context = context;
         }
 
+        /// <summary>
+        /// Gets all accounts.
+        /// </summary>
+        /// <response code="200">Returns all accounts</response>
         // GET: api/accounts
         [HttpGet("")]
+        [Produces("application/json")]
         public async Task<ActionResult<IEnumerable<Account>>> GetAll()
         {
             var accounts = await this.context.Accounts
@@ -34,8 +39,16 @@ namespace Finances.Controllers
             return accounts;
         }
 
+        /// <summary>
+        /// Gets a specific account.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <response code="200">Returns the requested account</response>
+        /// <response code="404">The requested account was not found</response>
         // GET: api/accounts/5
         [HttpGet("{id}")]
+        [Produces("application/json")]
+        [ProducesErrorResponseType(typeof(void))]
         public async Task<ActionResult<Account>> Get(int id)
         {
             var account = await this.context.Accounts
@@ -47,8 +60,32 @@ namespace Finances.Controllers
             return account;
         }
 
+        /// <summary>
+        /// Creates new account.
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /api/accounts
+        ///     {
+        ///         "lastName": "Иванов",
+        ///         "firstName": "Иван",
+        ///         "middleName": "Иванович",
+        ///         "birthDate": "1984-11-01",
+        ///         "balance": 200.00
+        ///     }
+        ///
+        /// </remarks>
+        /// <param name="model"></param>
+        /// <response code="201">Account is successfully created</response>
+        /// <response code="400">Invalid data is provided</response>
         // POST: api/accounts
         [HttpPost("")]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [ProducesErrorResponseType(typeof(void))]
         public async Task<ActionResult<Account>> Register(AccountCreateModel model)
         {
             if (model.Balance < 0)
@@ -72,8 +109,35 @@ namespace Finances.Controllers
             return this.BadRequest(this.ModelState);
         }
 
+        /// <summary>
+        /// Edits a specific account.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="model"></param>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     PUT /api/accounts/1
+        ///     {
+        ///         "lastName": "Иванов",
+        ///         "firstName": "Иван",
+        ///         "middleName": "Иванович",
+        ///         "birthDate": "1984-11-01",
+        ///         "balance": 200.00
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="204">Account is successfully updated</response>
+        /// <response code="400">Invalid data is provided</response>
+        /// <response code="404">The requested account was not found</response>
         // PUT: api/accounts/5
         [HttpPut("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        [ProducesErrorResponseType(typeof(void))]
         public async Task<ActionResult> Edit(int id, AccountEditModel model)
         {
             var account = await this.context.Accounts
@@ -97,19 +161,41 @@ namespace Finances.Controllers
             return BadRequest(this.ModelState);
         }
 
+        /// <summary>
+        /// Deletes a specific account.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <response code="204">Account is successfully deleted</response>
+        /// <response code="404">The requested account was not found</response>
         // DELETE: api/accounts/5
         [HttpDelete("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        [Produces("application/json")]
+        [ProducesErrorResponseType(typeof(void))]
         public async Task<ActionResult> Delete(int id)
         {
             var account = await context.Accounts
                 .FirstOrDefaultAsync(x => x.Id == id);
+            if (account == null)
+            {
+                return NotFound();
+            }
             context.Accounts.Remove(account);
             await context.SaveChangesAsync();
             return NoContent();
         }
 
+        /// <summary>
+        /// Returns balance of a specific account.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <response code="200">Returns the balance of the requested account</response>
+        /// <response code="404">The requested account was not found</response>
         // GET: api/accounts/5/balance
         [HttpGet("{id}/balance")]
+        [Produces("application/json")]
+        [ProducesErrorResponseType(typeof(void))]
         public async Task<ActionResult<decimal>> GetBalance(int id)
         {
             var account = await this.context.Accounts
@@ -121,8 +207,22 @@ namespace Finances.Controllers
             return account.Balance;
         }
 
+        /// <summary>
+        /// Deposits sum on a specific account.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="sum"></param>
+        /// <response code="204">The sum is successfully deposited</response>
+        /// <response code="400">Invalid data is provided</response>
+        /// <response code="404">The requested account was not found</response>
         //POST: api/accounts/5/deposit
         [HttpPost("{id}/deposit")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [Consumes("text/plain")]
+        [Produces("application/json")]
+        [ProducesErrorResponseType(typeof(void))]
         public async Task<ActionResult> Deposit(int id, [FromBody] decimal sum)
         {
             var account = await this.context.Accounts
@@ -152,8 +252,22 @@ namespace Finances.Controllers
             }
         }
 
+        /// <summary>
+        /// Withdraws sum from a specific account.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="sum"></param>
+        /// <response code="204">The sum is successfully withdrawn</response>
+        /// <response code="400">Invalid data is provided</response>
+        /// <response code="404">The requested account was not found</response>
         //POST: api/accounts/5/withdraw
         [HttpPost("{id}/withdraw")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [Consumes("text/plain")]
+        [Produces("application/json")]
+        [ProducesErrorResponseType(typeof(void))]
         public async Task<ActionResult> Withdraw(int id, [FromBody] decimal sum)
         {
             var account = await this.context.Accounts
--- Finances/Startup.cs
+++ Finances/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Finances.Data;
 using Microsoft.AspNetCore.Builder;
@@ -11,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Finances
 {
@@ -30,6 +33,16 @@ namespace Finances
                 options.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Finances API", Version = "v1" });
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +52,16 @@ namespace Finances
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseMvc();
         }
